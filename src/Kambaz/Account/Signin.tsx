e86e69dd46f01256c07541cc3eab7f7231d2dbd8
psadaphal99 <sadaphal.p@northeastern.edu>
2025-04-20 23:54:58 -0400

    project names info

--- src/Kambaz/Account/Signin.tsx
+++ src/Kambaz/Account/Signin.tsx
@@ -25,5 +25,38 @@ export default function Signin() {
         className="form-control mb-2" placeholder="password" type="password" id="wd-password" />
       <button onClick={signin} id="wd-signin-btn" className="btn btn-primary w-100" > Sign in </button>
       <Link id="wd-signup-link" to="/Kambaz/Account/Signup">Sign up</Link>
+
+
+      <hr className="my-4" />
+      <div id="wd-project-info">
+        <h5>Team Members</h5>
+        <ul>
+          <li>Pushkar Sadaphal - Section 1</li>
+          <li>Siddharth Sanjeev - Section 1</li>
+          <li>Sagar Bilwal - Section 1</li>
+        </ul>
+
+        <h5>Project Repositories</h5>
+        <ul>
+          <li>
+            <a
+              href="https://github.com/sidd36/kambaz-webdev-project-react/tree/quiz-merged"
+              target="_blank"
+              rel="noreferrer"
+            >
+              React.js Frontend Repository
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/sidd36/kambaz-webdev-project-node/tree/quiz-merged"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Node.js Backend Repository
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>);
 }
